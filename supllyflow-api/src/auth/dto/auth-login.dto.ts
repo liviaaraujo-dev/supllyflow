c5d951e15c2e9f285e683e78905bed52c869f6f5
livia araujo <liviaaraujo.dev@gmail.com>
2023-09-02 16:20:28 -0300

    create login

---
+++ supllyflow-api/src/auth/dto/auth-login.dto.ts
@@ -0,0 +1,17 @@
+import {
+    IsEmail,
+    IsNotEmpty,
+    IsString,
+  } from 'class-validator';
+  
+  export class AuthLoginDto {
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+  
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+
+  }
+  
